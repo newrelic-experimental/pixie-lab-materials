ee59a0644586596d44fe618e6c2434eb4ff93c6b
Alex Ronquillo <aronquillo@newrelic.com>
2021-10-06 15:11:02 -0400

    Update fetch-2

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ echo "Please wait while we update your lab environment."
 
 kubectl apply -f kube/fetch-2.yaml
 pod=$(kubectl get pods -o name | grep mysql)
-kubectl exec -it $pod -- bash -c "mysql --database=main --password=password -e \"INSERT INTO images VALUES (2, 'pixie', 'https://tinyhats.s3.amazonaws.com/pixie.png', 'pixie.png', 'PIXIE', 'true');\"" &> /dev/null
+kubectl exec -it $pod -- bash -c "mysql --database=main --password=password -e \"INSERT INTO images VALUES (101, 'pixie', 'https://tinyhats.s3.amazonaws.com/pixie.png', 'pixie.png', 'PIXIE', 'true');\"" &> /dev/null
 
 echo "Done!"
