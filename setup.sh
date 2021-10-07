d886608d4becd1411f45bcae94a23663b082f35c
Alex Ronquillo <aronquillo@newrelic.com>
2021-10-07 14:55:49 -0400

    Use eks manifest

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Please wait while we update your lab environment."
 
-kubectl apply -f kube/fetch-2.yaml
+kubectl apply -f kube
 pod=$(kubectl get pods -o name | grep mysql)
 kubectl exec -it $pod -- bash -c "mysql --database=main --password=password -e \"INSERT INTO images VALUES (101, 'pixie', 'https://tinyhats.s3.amazonaws.com/pixie.png', 'pixie.png', 'PIXIE', 'true');\"" &> /dev/null
 
