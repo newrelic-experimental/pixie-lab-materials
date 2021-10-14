bd14998e54a89b9493b9a6322ff194ac8c6f14cc
Alex Ronquillo <aronquillo@newrelic.com>
2021-10-14 15:32:39 -0400

    remove teardown

--- teardown.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "Please wait while we reset your lab environment."
-
-kubectl apply -f kube/fetch.yaml
-pod=$(kubectl get pods -o name | grep mysql)
-kubectl exec -it $pod -- bash -c "mysql --database=main --password=password -e \"DELETE FROM images WHERE id = 101;\"" &> /dev/null
-
-echo "Done!"
