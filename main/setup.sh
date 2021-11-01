4a144bf3e1bdd13ed939ab809e427ef6401d8f4b
Alex Ronquillo <aronquillo@newrelic.com>
2021-11-01 16:55:07 -0400

    Push simulators to docker hub

--- main/setup.sh
+++ main/setup.sh
@@ -36,6 +36,4 @@ fi
 
 echo -e "\n=== Spinning up your cluster ==="
 minikube start --driver=hyperkit --cni=flannel --cpus=4 --memory=8000 -p minikube-pixie-lab
-eval $(minikube docker-env -p minikube-pixie-lab)
-docker build simulator -t simulator
 kubectl apply -f kube
--- scenario-2/setup.sh
+++ scenario-2/setup.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 echo "Please wait while we update your lab environment."
 
-eval $(minikube docker-env -p minikube-pixie-lab)
-docker build simulator -t simulator-scenario-2
-
 pod=$(kubectl get pods -o name | grep mysql)
 kubectl exec -it $pod -- bash -c "mysql --database=main --password=password -e \"UPDATE images SET approve = 'false' WHERE id = 101;\"" &> /dev/null
 kubectl apply -f kube
