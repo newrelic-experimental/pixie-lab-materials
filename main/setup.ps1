2773cb1c6158b180713182990b08ce291178a3a4
pnvnd <peter@datacrunch.ca>
2021-11-04 18:49:04 -0400

    Added PowerShell scripts for Windows users

---
+++ main/setup.ps1
@@ -0,0 +1,29 @@
+﻿echo "Please wait while we set up your lab environment.`n"
+
+echo "=== Checking your dependencies ==="
+
+$error.clear()
+try { minikube version > $null }
+catch { "Error occured" }
+if (!$error) { "minikube ✓" } else { echo "Please install minikube" }
+
+$error.clear()
+try { kubectl version --client=true > $null }
+catch { "not found" }
+if (!$error) { "kubectl ✓" } else { echo "Please install kubectl" }
+
+$hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
+if ($hyperv.State -eq "Enabled") { "Hyper-V ✓" } else { echo "Please install Hyper-V or Start Hyper-V Manager Service" }
+
+if ((Get-VMSwitch | findstr -i external) -eq $null) { "Please create an External Hyper-V Switch" } else { echo "Hyper-V External Switch ✓" }
+
+$error.clear()
+try { helm version > $null }
+catch { "Error occured" }
+if (!$error) { "helm ✓" } else { echo "Please install helm" }
+
+echo "`n=== Spinning up your cluster ==="
+
+# 8000 MB memory is not enough on Windows and 4 CPUs runs a bit slow
+minikube start --driver=hyperv --hyperv-virtual-switch='New Virtual Switch' --cni=flannel --cpus=6 --memory=10000 -p minikube-pixie-lab
+kubectl apply -f kube
---
+++ scenario-2/setup.ps1
@@ -0,0 +1,9 @@
+﻿echo "Please wait while we set up your lab environment.`n"
+
+$pod=kubectl get pods -o custom-columns=":metadata.name" | findstr mysql
+
+$query="mysql --database=main --password=password --execute=\""UPDATE images SET approve = 'false' WHERE id = 101;"""
+kubectl exec -it $pod -- bash -c $query > $null
+kubectl apply -f kube
+
+echo "Done!"
